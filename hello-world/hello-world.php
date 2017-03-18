b3580bd329bd2b31567c8d1eff4a20b4cba8dfbb
Ahmed Khalaf <pxlshpr@gmail.com>
2017-03-18 09:41:18 +0800

    add a hello world plugin

---
+++ hello-world/hello-world.php
@@ -0,0 +1,11 @@
+<?php
+   /*
+   Plugin Name: Hello World
+   Plugin URI: url-goes-here
+   Description: A hello world plugin
+   Version: 0.1k
+   Author: pxlshpr
+   Author URI: url-goes-here
+   License: GPL2
+   */
+?>
